d0e437a669ecdea0b16d592144061369374df215
insanexyz <shreyanshsaha19@proton.me>
2025-12-06 14:38:28 +0530

    Update ui and game theme

--- script.js
+++ script.js
@@ -1,55 +1,95 @@
-const humanChoiceButtons = document.querySelector(".human-choice-buttons");
-const computerChoiceDisplayArea = document.querySelector(".computer-choice-display-area");
-const humanChoiceDisplayArea = document.querySelector(".human-choice-display-area");
-const whoWonArea = document.querySelector(".who-won-area");
-const clearButton = document.querySelector(".clear-btn");
-const textArea = document.querySelector(".text-area");
+const btnContainer = document.querySelector(".button-container");
+const playerResultEmoji = document.querySelector(".player-result .result-emoji");
+const playerResultText = document.querySelector(".player-result-text");
+const computerResultEmoji = document.querySelector(".computer-result .result-emoji");
+const computerResultText = document.querySelector(".computer-result-text");
+const roundsText = document.querySelector(".rounds");
+const battleInfo = document.querySelector(".battle-info");
+const displayRules = document.querySelector(".display-rules");
+const rulesBtn = document.querySelector(".rules-open-btn");
+const rulesCloseBtn = document.querySelector(".rules-close-btn");
 
-let humanChoice = 0;
-let computerChoice = 0;
-let humanScore = 0;
+let computerChoice = "";
+let playerChoice = "";
+let playerScore = 0;
 let computerScore = 0;
+let rounds = 0;
+let playerWins = false;
+
+const winMessages = {
+  dragon: "Dragon burned the knight",
+  knight: "Knight slaughtered the wizard",
+  wizard: "Wizard enchanted the dragon"
+};
+
+const loseMessages = {
+  dragon: "Dragon got enchanted by wizard",
+  knight: "Knight got burnt by the dragon",
+  wizard: "Wizard got slaughtered by the knight"
+};
 
 function getComputerChoice() {
-    let rn = Math.floor(Math.random() * 3); // returns random number from 0 to 2
-    switch (rn) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissor";
-    }
+  let rn = Math.floor(Math.random() * 3); // returns random number from 0 to 2
+  switch (rn) {
+    case 0: return "dragon";
+    case 1: return "knight";
+    case 2: return "wizard";
+  }
 }
 
 
-humanChoiceButtons.addEventListener("click", (event) => {
-  humanChoice = event.target.id;
-  humanChoiceDisplayArea.textContent = "You chose " + humanChoice;
-  playRound(humanChoice, computerChoice);
+btnContainer.addEventListener("click", (event) => {
+  playerChoice = event.target.id;
+  playerChoiceEmoji = event.target.textContent;
+  playerResultEmoji.textContent = playerChoiceEmoji;
+  playRound(playerChoice, computerChoice);
 })
 
 
-clearButton.addEventListener("click", () => {
-    const textAreaChildrenElements = textArea.children;
-    for (let i = 0; i < textAreaChildrenElements.length; i++) {
-        textAreaChildrenElements[i].textContent = "";
-    }
-})
+function playRound(playerChoice, computerChoice) {
+  computerChoice = getComputerChoice();
 
+  switch (computerChoice) {
+    case "dragon": computerResultEmoji.textContent = "🐉"; break;
+    case "knight": computerResultEmoji.textContent = "⚔️"; break;
+    case "wizard": computerResultEmoji.textContent = "🪄"; break;
+  }
 
-function playRound(humanChoice, computerChoice) {
-    computerChoice = getComputerChoice();
-    computerChoiceDisplayArea.textContent = "Computer has chosen " + computerChoice;
+  rounds++;
+  roundsText.textContent = `Rounds: ${rounds}`;
 
-    if (humanChoice == computerChoice) {
-        whoWonArea.textContent = "Tie";
-    }
-    else if ((humanChoice == "rock" && computerChoice == "scissor") ||
-        (humanChoice == "paper" && computerChoice == "rock") ||
-        (humanChoice == "scissor" && computerChoice == "paper")) {
-        whoWonArea.textContent = "You won";
-        humanScore++;
-    } else {
-        whoWonArea.textContent = "Computer won";
-        computerScore++;
-    }
+  // Game logic
+  if (playerChoice == computerChoice) {
+    battleInfo.textContent = "It's a tie";
+    return;
+  }
+  else if ((playerChoice === "dragon" && computerChoice === "knight") ||
+    (playerChoice === "knight" && computerChoice === "wizard") ||
+    (playerChoice === "wizard" && computerChoice === "dragon")) {
+    playerWins = true;
+  } else {
+    playerWins = false;
+  }
+
+  if (playerWins) {
+    battleInfo.textContent = winMessages[playerChoice];
+    playerScore++;
+    playerResultText.textContent = `Player: ${playerScore}`;
+  } else {
+    battleInfo.textContent = loseMessages[playerChoice];
+    computerScore++;
+    computerResultText.textContent = `Computer: ${computerScore}`;
+  }
 }
+
+
+rulesBtn.addEventListener("click", () => {
+  displayRules.classList.add("show");
+})
+
+rulesCloseBtn.addEventListener("click", () => {
+  displayRules.classList.remove("show");
+})
+
+
 
